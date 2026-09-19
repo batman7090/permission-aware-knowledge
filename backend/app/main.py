336c97301a719d69303951f4be0780bd3bfe1da1
K Shamanth Kumar <kshamanth97@gmail.com>
2026-09-19 11:14:27 +0200

    feat: addition of debug code to fastapi

--- backend/app/main.py
+++ backend/app/main.py
@@ -68,3 +68,9 @@ def user_documents(
             for document in documents
         ],
     }
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
